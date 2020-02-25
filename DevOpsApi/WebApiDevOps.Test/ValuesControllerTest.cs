127a489567145e15ba07e1bfefa4ebb7c49b2892
Alfredo Sanchez Alvarez <alfredo.sanchez@avantica.net>
2020-02-24 19:21:09 -0500

    initial commit

---
+++ DevOpsApi/WebApiDevOps.Test/ValuesControllerTest.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using WebApiDevOps.Controllers;
+using Xunit;
+
+namespace WebApiDevOps.Test
+{
+    public class ValuesControllerTest
+    {
+        ValuesController _controller;
+        public ValuesControllerTest()
+        {
+            _controller = new ValuesController();
+        }
+
+        [Fact]
+        public void Test_Get_Values()
+        {
+            //Arrange
+            string[] expected = new string[] { "value1", "value2" };
+
+            // Act
+            var result = _controller.Get();
+
+            // Assert
+            Assert.Equal(expected, result.Value);
+        }
+    }
+}
